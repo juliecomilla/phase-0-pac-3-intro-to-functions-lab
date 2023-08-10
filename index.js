1a0a3adcb95202fa6174a5d1827b09b10e644d08
Julie <juliecomilla@gmail.com>
2023-08-10 13:01:43 -0400

    completed lab

---
+++ index.js
@@ -0,0 +1,25 @@
+function shout(string) {
+    return string.toUpperCase();
+  }
+  function whisper(string) {
+    return string.toLowerCase();
+  }
+  function logShout(string){
+    console.log(string.toUpperCase())
+  }
+  function logWhisper(string){
+    console.log(string.toLowerCase())
+  }
+  function sayHiToHeadphonedRoommate(string) { 
+const canthearyou = "I can\'t hear you!";
+const yesindeed = "YES INDEED!"
+const dintogether = "I would love to!";
+if (string.toLowerCase(string) === string)
+return canthearyou;
+  
+if (string.toUpperCase(string) === string)
+return yesindeed;
+
+if ("Let\'s have dinner together!" === string)
+return dintogether;
+  }
